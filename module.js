d23bb01a22a8a04372e3e637e97ec61500c22c4d
Grindstaff <nagr225@uky.edu>
2024-03-28 13:19:15 -0400

    cleaned it up a little and added the rest of the sections

--- module.js
+++ module.js
@@ -1,23 +1,32 @@
 import { json } from './json1.js';
 
 function generateNav() {
-  const navContainer = document.getElementById('main-nav');
 
-  // Iterating over the 'About' section
+  const navContainer = document.createElement('nav');
+  navContainer.classList.add('bg-blue-800', "px-4", "py-8", "flex", "gap-8", "text-white");
+  document.body.insertBefore(navContainer, document.body.firstChild);
+
+  const aboutContainer = document.createElement('ul');
+  aboutContainer.setAttribute("id", "aboutContainer");
+  navContainer.appendChild(aboutContainer);
+  
   Object.entries(json.About).forEach(([key, value]) => {
-    const spanElement = document.createElement('span');
-    spanElement.innerHTML = `<a href="${value}">${key}</a>`;
-    navContainer.appendChild(spanElement);
+    const liElement = document.createElement('li');
+    liElement.innerHTML=` <a href="${value}">${key}</a>`
+    aboutContainer.appendChild(liElement)
+  });
+  
+  const howToContainer = document.createElement('ul');
+  howToContainer.setAttribute("id", "howToContainer");
+  navContainer.appendChild(howToContainer);
+
+  Object.entries(json['How to']).forEach(([key, value]) => {
+    const liElement = document.createElement('li');
+    liElement.innerHTML=` <a href="${value}">${key}</a>`
+    howToContainer.appendChild(liElement)
   });
 
-  // Iterating over the 'How to' section
-  Object.entries(json["How to"]).forEach(([key, value]) => {
-    const spanElement = document.createElement('span');
-    spanElement.innerHTML = `<a href="${value}">${key}</a>`;
-    navContainer.appendChild(spanElement);
-  });
 
-  // Adding the login link
   const loginSpan = document.createElement('span');
   loginSpan.innerHTML = `<a href="${json.login}">Login</a>`;
   navContainer.appendChild(loginSpan);
